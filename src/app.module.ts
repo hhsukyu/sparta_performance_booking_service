59bbc45cdbc326b623fbcb1d505bd9dbe941b827
hhsukyu <hhsukyu@gmail.com>
2024-01-03 04:00:14 +0900

    feat:[user] 유저 mo,s,co 생성 및 엔티티 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 const typeOrmModuleOptions = {
   useFactory: async (
@@ -40,6 +41,7 @@ const typeOrmModuleOptions = {
     }),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
     AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Column,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+import { Role } from '../types/userRole.type';
+
+@Index('email', ['email'], { unique: true })
+@Entity({
+  name: 'users',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', select: false, nullable: false })
+  password: string;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  nickName: string;
+
+  @Column({ type: 'int', default: 1000000 })
+  point: number;
+
+  @Column({ type: 'enum', enum: Role, default: Role.User })
+  role: Role;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt?: Date;
+}
